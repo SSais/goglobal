dcdb659991c5ab938bfeb89be65bac650c4c43be
rw4n99 <rowangodfrey2@gmail.com>
2024-11-27 15:39:40 +0000

    Added auth

--- app/app/_layout.tsx
+++ app/app/_layout.tsx
@@ -1,11 +1,10 @@
 import { Stack } from "expo-router";
 import { Link } from "expo-router";
-import login from "./login";
 
 export default function RootLayout() {
   return (
     <Stack>
-      <Link href="/login">About</Link>
+      <Link href="/auth">About</Link>
     </Stack>
   );
 }
--- app/app/index.tsx
+++ app/app/index.tsx
@@ -1,17 +1,27 @@
-import { Text, View } from "react-native";
-import { Link } from "expo-router";
+import 'react-native-url-polyfill/auto'
+import { useState, useEffect } from 'react'
+import { supabase } from '../lib/supabase'
+import Auth from '../components/Auth/Auth'
+import { View, Text } from 'react-native'
+import { Session } from '@supabase/supabase-js'
 
-export default function Index() {
+export default function App() {
+  const [session, setSession] = useState<Session | null>(null)
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session)
+    })
+
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session)
+    })
+  }, [])
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-      <Link href="/login">login</Link>
+    <View>
+      <Auth />
+      {session && session.user && <Text>{session.user.id}</Text>}
     </View>
-  );
+  )
 }
