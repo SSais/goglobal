64b3d74dad519405e44086035ab8e8f7d2a65076
rw4n99 <rowangodfrey2@gmail.com>
2024-12-04 13:48:30 +0000

    Added dropdown

--- app/app/newappli/index.tsx
+++ app/app/newappli/index.tsx
@@ -1,45 +1,57 @@
-import { useState, useEffect } from 'react'
-import { Alert, View, Text } from 'react-native'
-import { supabase } from '../../lib/supabase'
+import { useState, useEffect } from 'react';
+import { Alert, View, Text } from 'react-native';
+import { supabase } from '../../lib/supabase';
 import { Link } from 'expo-router';
 
 import Header from "@/components/DashBoard/Header";
+import Questions from '@/components/Questions/Questions';
 
-export default function Newappli () {
-    interface Profile {
-        id: string;
-        first_name: string;
-        last_name: string;
-        phone: string;
-        dob: string;
+export default function Newappli() {
+  interface Profile {
+    id: string;
+    first_name: string;
+    last_name: string;
+    phone: string;
+    dob: string;
+  }
+
+  // interface questionData {
+  //   question: string;
+  //   answerType: string;
+  //   answerOptions: string[];
+  // }
+
+  const [profiles, setProfiles] = useState<Profile[]>([]);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchProfiles = async () => {
+      const { data, error } = await supabase.from('profiles').select('*');
+      if (error) {
+        setError(error.message);
+        Alert.alert('Error', error.message);
+      } else {
+        setProfiles(data || []);
       }
-    
-      const [profiles, setProfiles] = useState<Profile[]>([]);
-      const [error, setError] = useState<string | null>(null);
-    
-      useEffect(() => {
-        const fetchProfiles = async () => {
-          const { data, error } = await supabase.from('profiles').select('*');
-          if (error) {
-            setError(error.message);
-            Alert.alert('Error', error.message);
-          } else {
-            setProfiles(data || []);
-          }
-        };
-    
-        fetchProfiles();  
-        //a reminder for what is nbeing fetched with profiles
-        //console.log(profiles);
-      }, []);
-    
-      if (profiles.length === 0) {
-        return <Text>Loading...</Text>;
-      }
-  
-return (
-  <View>
-    <Header prof={profiles} />
-    <Text>Open Application</Text>
-  </View>
-)}
+    };
+
+    fetchProfiles();
+    //console.log(profiles);
+  }, []);
+
+  if (profiles.length === 0) {
+    return <Text>Loading...</Text>;
+  }
+
+  return (
+    <View>
+      <Header prof={profiles} />
+      <Text>Open Application</Text>
+      <Questions 
+        // question={'What passport/s do you hold?'} 
+        // answerOptions={['Australian', 'British', 'Canadian', 'American', 'Other']} 
+        // answerType={'dropdown'}
+      />
+    </View>
+  );
+}
